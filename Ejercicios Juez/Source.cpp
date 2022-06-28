d9492dc22784755e1ec567e736ccff8ec1db8b0b
LordSama <72462333+LordSama@users.noreply.github.com>
2022-06-28 11:04:30 +0200

    Add files via upload

---
+++ Ejercicios Juez/Source.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "Queue.h"
+using namespace std;
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int aux;
+
+    cin >> aux;
+    if (!std::cin)
+        return false;
+
+    Queue<int> cola;
+    while (aux != 0) {
+        cola.push_back(aux);
+        cin >> aux;
+    }
+
+    cola.invertir();
+
+    // escribir sol
+    cout << cola << endl;
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
